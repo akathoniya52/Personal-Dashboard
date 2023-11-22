681c24d25140d6ed1ac5afb1ea6a75118c361686
Amit patel <54068272+akathoniya52@users.noreply.github.com>
2023-11-22 21:59:47 +0530

    Update DashBoard.js

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -4,7 +4,7 @@ import tasks from "./Tasks";
 export default function DashBoard() {
   return (
     <>
-      <h1>Personal DashBoard</h1>
+      <h1 className="text-center text-[25px] font-bold">Personal DashBoard</h1>
       <div className="tasks">
         {tasks.map((task) => (
           <Tasks key={task.id} taskNumber={task.id} taskName={task.taskName} />
